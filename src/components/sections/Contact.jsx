13733799ab3535ba72e2f4198654b218445b52d9
yeathuku <unkuyeathuku@gmail.com>
2025-05-10 22:58:56 +0530

    Initial commit

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
 import { RevealOnScroll } from "./RevealOnScroll";
+import emailjs from "emailjs-com";
 export const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_SERVICE_ID,
+        import.meta.env.VITE_TEMPLATE_ID,
+        e.target,
+        import.meta.env.VITE_PUBLIC_KEY
+      )
+      .then((result) => {
+        alert("message sent");
+        setFormData({ name: "", email: "", message: "" });
+      })
+      .catch(() => alert("something went wrong .please try again"));
+  };
   return (
     <section
       id="contact"
@@ -10,13 +32,17 @@ export const Contact = () => {
           <h1 className="text-3xl font-bold mb-8 bg-gradient-to-r from-red-900 to-red-500 bg-clip-text text-transparent text-center">
             For contact
           </h1>
-          <form className="space-y-6">
+          <form className="space-y-6" onSubmit={handlesubmit}>
             <div className="relative">
               <input
                 type="text"
                 id="name"
                 required
+                value={formData.name}
                 placeholder="Name"
+                onChange={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }
                 className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-red-900 focus:bg-blue-500/5"
               />
             </div>
@@ -25,7 +51,11 @@ export const Contact = () => {
                 type="text"
                 id="email"
                 required
+                value={formData.email}
                 placeholder="EMAIL-ID"
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
                 className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-red-900 focus:bg-blue-500/5"
               />
             </div>
@@ -34,8 +64,12 @@ export const Contact = () => {
                 name="message"
                 id="message"
                 required
+                value={formData.message}
                 placeholder="Message"
                 className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-red-900 focus:bg-blue-500/5"
+                onChange={(e) =>
+                  setFormData({ ...formData, message: e.target.value })
+                }
               />
             </div>
             <button
